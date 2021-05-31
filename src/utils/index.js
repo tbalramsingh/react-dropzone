cdbfe14812a3b8367a1b4617048c614b286c5859
Todd Balramsingh <todd.balramsingh@gettyimages.com>
2021-05-31 14:31:16 -0600

    allow html files

--- src/utils/index.js
+++ src/utils/index.js
@@ -89,6 +89,7 @@ export function isEvtWithFiles(event) {
   }
   // https://developer.mozilla.org/en-US/docs/Web/API/DataTransfer/types
   // https://developer.mozilla.org/en-US/docs/Web/API/HTML_Drag_and_Drop_API/Recommended_drag_types#file
+  return true;
   return Array.prototype.some.call(
     event.dataTransfer.types,
     type => type === 'Files' || type === 'application/x-moz-file'
